6ab9298ba9a5f63a8686144ca2db907821e7e107
Sneyder Ospina <sneydero@hotmail.com>
2024-08-22 22:18:17 -0500

    useMemo hook

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,11 @@ import MultipleCustomHook from "./03-examples/MultipleCustomHook";
 import FocusScreen from "./04-useRef/FocusScreen";
 import Layout from "./05-useLayoutEffect/Layout";
 import Memorize from "./06-memos/Memorize";
+import MemoHook from "./06-memos/MemoHook";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // <React.StrictMode>
   // <MultipleCustomHook />
-  <Memorize />
+  <MemoHook />
   //</React.StrictMode>
 );
